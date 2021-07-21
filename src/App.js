4505e6b2e16e97cc7e44f2663c517a0c2d5f77bf
Ashish Ranjan Kumar <imashish2589@gmail.com>
2021-07-22 05:07:03 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  function execute() {
+    var htmlCode = document.querySelector("#html-code").value;
+    var cssCode =
+      "<style>" + document.querySelector("#css-code").value + "</style>";
+    var jsCode = document.querySelector("#js-code").value;
+    var iframe = document.querySelector("#output");
+    document.querySelector("#output").contentDocument.body.innerHTML =
+      htmlCode + cssCode;
+    try {
+      iframe.contentWindow.eval(jsCode);
+    } catch (e) {}
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="editor">
+        <p className="header"> CodeWeb </p>
+        <div className="column">
+          <div className="code-write">
+            <div className="row">
+              <textarea
+                id="html-code"
+                spellCheck="false"
+                className="html"
+                placeholder="Write html code here"
+                onInput={execute}
+              ></textarea>
+              <div className="css-js">
+                <textarea
+                  id="css-code"
+                  spellCheck="false"
+                  className="css"
+                  onInput={execute}
+                  placeholder="Write css code here"
+                />
+                <textarea
+                  id="js-code"
+                  spellCheck="false"
+                  className="js"
+                  onInput={execute}
+                  placeholder="Write js code here(Don't write script tag)"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="code-output">
+            <iframe title="output" id="output"></iframe>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
